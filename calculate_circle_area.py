258880db87c3eebdfb3b2d6cc09d2be4b1b0f43c
Abhinav Mittal <abhinavmittal93@gmail.com>
2021-05-25 14:07:18 -0400

    Refactored the method name and called the 2 modules from a single module

--- calculate_circle_area.py
+++ calculate_circle_area.py
@@ -1,7 +1,7 @@
 import math
 
 
-def calc_radius():
+def calc_area():
     radius = float(input('Enter the radius of the circle: '))
     area = math.pi * radius ** 2
     return f'Area of the circle is: {area:.2f}'
---
+++ main.py
@@ -0,0 +1,8 @@
+import current_date_time
+import calculate_circle_area
+
+
+print(f'Current date and time: {current_date_time.get_current_date_time()}')
+
+
+print(calculate_circle_area.calc_area())
